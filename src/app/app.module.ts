4be78bf57ea3278f43601d57c8a201c04e71b646
Jonas Oliveira <jloide.essence@rd.com.br>
2020-08-18 17:25:02 -0300

    adicionando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProdutoComponent,
-    ClienteComponent
+    ClienteComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -8,8 +8,6 @@ import { Produto } from './produto.model';
 })
 export class ProdutoComponent implements OnInit {
 
-  @Input() produto: Produto
-
   constructor() { }
 
   ngOnInit(): void {
@@ -19,5 +17,29 @@ export class ProdutoComponent implements OnInit {
   clicked():void {
     console.log("opa! Clicou");  
   }
+
+  produtos: Produto[] = [
+    {
+      id:"123", 
+      nome: "Brastemp B34", 
+      temEsoque: true, 
+      descricao: "Muito bom",
+      img: "https://compracerta.vteximg.com.br/arquivos/ids/307851/Geladeira-BRE80AK-Frontal.jpg?v=637298145010330000"
+    },
+    {
+      id:"1134324", 
+      nome: "Consul c789", 
+      temEsoque: false, 
+      descricao: "Manda para Conchal",
+      img: "https://static.carrefour.com.br/medias/sys_master/images/images/h79/h87/h00/h00/13404723216414.jpg"
+    },
+    {
+      id:"1134324", 
+      nome: "Dell AT45", 
+      temEsoque: true, 
+      descricao: "Manda para Conchal",
+      img: "https://http2.mlstatic.com/notebook-dell-inspiron-i15-7580-m30s-ci7-8gb-ssd-156-win10-D_NQ_NP_909323-MLB31090463443_062019-F.jpg"
+    }
+  ]
 
 }
