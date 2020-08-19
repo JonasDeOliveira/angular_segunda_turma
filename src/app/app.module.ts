8bdcd84bbf55fe2530b38feeddb6b8e26925141d
Jonas Oliveira <jloide.essence@rd.com.br>
2020-08-19 11:58:39 -0300

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR')
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +31,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -9,6 +9,9 @@ export class CadastroComponent implements OnInit {
 
   constructor() { }
 
+  pessoa = {nome: "Jonas", nasc: "1993-09-01"};
+  curso = {concluido: 0.5, valor: 3000};
+
   ngOnInit(): void {
   }
 
